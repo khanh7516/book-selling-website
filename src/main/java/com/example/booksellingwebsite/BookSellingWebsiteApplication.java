f01776d9e51f39118c4fa31af978ff8710266b55
KhanhNgo <62138525+khanh7516@users.noreply.github.com>
2023-11-01 02:50:33 +0700

    Init project

---
+++ src/main/java/com/example/booksellingwebsite/BookSellingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksellingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSellingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSellingWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booksellingwebsite/BookSellingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksellingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSellingWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
