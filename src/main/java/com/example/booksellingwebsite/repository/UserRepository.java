b25b70601a82141daa2d6ce7ba6a28b5641a0970
KhanhNgo <62138525+khanh7516@users.noreply.github.com>
2023-11-25 03:49:38 +0700

    Summary

--- src/main/java/com/example/booksellingwebsite/repository/UserRepository.java
+++ src/main/java/com/example/booksellingwebsite/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.example.booksellingwebsite.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    public Long countById(Integer id);
+
 }
